aa817b0fa0f81119331b73e11912f5f16537874f
ujjwolkhatri <ujjwolkhatri@192.168.0.104>
2026-04-06 19:59:17 +0545

    used multithreading to optmize api fetch

--- etl.py
+++ etl.py
@@ -4,7 +4,9 @@ from sqlalchemy import create_engine
 from countrystatecity_countries import get_cities_of_country,get_countries
 import json
 import time
-# from multiprocessing import Pool
+from concurrent.futures import ThreadPoolExecutor
+
+
 
 
 ENGINE = create_engine("postgresql://ujjwolkhatri:password@localhost:5432/weather_db")
@@ -57,7 +59,7 @@ def load_dataframe_to_postgres(dataframe: pd.DataFrame, table_name: str, if_exis
 
 
 def get_lat_long():
-    query = "select latitude,longitude from weather_schema.city_list limit 1;"
+    query = "select latitude,longitude from weather_schema.city_list ;"
     df = get_dataframe_from_postgres(query=query)
     return df
     
@@ -78,28 +80,34 @@ def api_url(df):
     # print(ll_url,"hi")
     return ll_url
 
-def extract_weather_data(url):
-    data=[]
+# def extract_weather_data(url):
+#     data=[]
 
-    for url in url:
-        df = requests.get(url).json()
-        # print(df)
-        data.append(df)
+#     for url in url:
+#         df = requests.get(url).json()
+#         # print(df)
+#         data.append(df)
         
-    return data
+#     return data
+
+
+
+
 
 def convert_json_columns(df):
     for col in df.columns:
-        df[col] = df[col].apply(
-            lambda x: json.dumps(x) if isinstance(x, (dict, list)) else x
-        )
+        df[col] = df[col].apply(json.dumps)
+        df = df.where(df.notnull(), None)
     return df
 
 chunk_size = 100
 def chunk_url(ll_url,size):
     for i in range(0,len(ll_url),size):
-        yield ll_url[18900:i+size]
+        yield ll_url[i:i+size]
 
+def fetch(url):
+    response=requests.get(url).json()
+    return response
 
 if __name__ == "__main__":
 
@@ -109,26 +117,34 @@ if __name__ == "__main__":
     # load_dataframe_to_postgres(city_name,"city_list","replace")
     df_lat_lon= get_lat_long()
     url=api_url(df=df_lat_lon)
-    # print(url)
-    # total_time= 0.0
-    # for url in url:
-    #     response = requests.get(url)
-    #     elapsed_seconds = response.elapsed.total_seconds()
-    #     # print(f"API response time: {elapsed_seconds} seconds")
+    with ThreadPoolExecutor(max_workers=9) as executor:
+        for chunk in chunk_url(ll_url=url,size=500):
+            results = executor.map(fetch, chunk)
+            data=(list(results))
+            df=pd.DataFrame(data)
+            df=convert_json_columns(df=df)
+            # print(df)
+            load_dataframe_to_postgres(df,"weather_new_data","append")
+        # for data in data:
+        #     data=pd.DataFrame
+        #     df=convert_json_columns(data)
+        #     print(df)
+        # load_dataframe_to_postgres(df,"weather_new_data","replace")
 
-    #     total_time += elapsed_seconds  # add float
-    # print(total_time)
-        
 
     
-    data=chunk_url(ll_url=url,size=chunk_size)
+    
+        
+      
 
-    for i in data:
-        data=extract_weather_data(i)
-        df=pd.DataFrame(data)
-        df = convert_json_columns(df)
-        df.to_csv("output.csv",mode="a", index=False)
-        load_dataframe_to_postgres(df,"weather_new_data","append")
+
+
+
+   
+
+    
+        
+    
 
 
     
